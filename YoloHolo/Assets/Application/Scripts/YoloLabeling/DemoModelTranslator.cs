6e82cb6e9b1526759cb075422c9ee43dd9590534
Rui Li <125332214+Real3Lee@users.noreply.github.com>
2024-03-31 15:26:26 -0400

    holo

---
+++ YoloHolo/Assets/Application/Scripts/YoloLabeling/DemoModelTranslator.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using YoloHolo.Services;
+
+namespace YoloHolo.YoloLabeling
+{
+    [Serializable]
+    public class DemoModelTranslator : IYoloClassTranslator
+    {
+        public string GetName(int classIndex)
+        {
+            return detectableObjects[classIndex];
+        }
+
+        private static List<string> detectableObjects = new()
+        {
+            "Rover",
+            "Leo Rover"
+        };
+    }
+}
